e208a4ecf53aad9a930f4dc25d38f5b267bf2994
user <magzhankabi@gmail.com>
2024-11-29 21:07:58 +0500

    StudentList

--- src/main/java/kz/bitlab/springbootstudy/db/DBManager.java
+++ src/main/java/kz/bitlab/springbootstudy/db/DBManager.java
@@ -12,7 +12,7 @@ public class DBManager {
     static {
         students.add(new Student(1L, "Sanzhar", "S", 80, "B"));
         students.add(new Student(2L, "Kuat", "Abylay", 40, "D"));
-        students.add(new Student(3L, "Kazy", "M", 50, "D"));
-        students.add(new Student(4L, "Alisher", "A", 75, "B"));
+        students.add(new Student(3L, "Kazy", "M", 37, "D"));
+        students.add(new Student(4L, "Alisher", "A", 80, "B"));
     }
 }
