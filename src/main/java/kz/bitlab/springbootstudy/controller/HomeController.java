f5f4f732cfe495532b23acb821043b3bc6e0e837
user <magzhankabi@gmail.com>
2024-12-02 13:12:42 +0500

    StudentList

--- src/main/java/kz/bitlab/springbootstudy/controller/HomeController.java
+++ src/main/java/kz/bitlab/springbootstudy/controller/HomeController.java
@@ -5,6 +5,9 @@ import kz.bitlab.springbootstudy.entity.Student;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
 
@@ -15,5 +18,20 @@ public class HomeController {
         List<Student> students = DBManager.getStudents();
         model.addAttribute("studentter", students);
         return "home";
+    }
+
+    @GetMapping("/addstudent.html")
+    public String showAddStudentPage() {
+        return "addstudent";
+    }
+    @PostMapping("/addstudent")
+    public String addStudent(@RequestParam(name = "student_name") String name,
+                             @RequestParam(name = "student_surname") String surname,
+                             @RequestParam(name = "student_exam") int exam,
+                             @RequestParam(name = "student_mark") String mark) {
+
+        Student newStudent = new Student(null, name, surname, exam, mark);
+        DBManager.addStudent(newStudent);
+        return "redirect:/";
     }
 }
--- src/main/java/kz/bitlab/springbootstudy/db/DBManager.java
+++ src/main/java/kz/bitlab/springbootstudy/db/DBManager.java
@@ -8,12 +8,20 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class DBManager {
-    @Getter
     private static final List<Student> students = new ArrayList<>();
     static {
         students.add(new Student(1L, "Sanzhar", "S", 80, "B"));
         students.add(new Student(2L, "Kuat", "Abylay", 40, "D"));
         students.add(new Student(3L, "Kazy", "M", 37, "D"));
         students.add(new Student(4L, "Alisher", "A", 80, "B"));
+    }
+    private static Long id=5L;
+    public static List<Student> getStudents() {
+        return students;
+    }
+    public static void addStudent(Student student) {
+        student.setId(id);
+        students.add(student);
+        id++;
     }
 }
